98f4301a80a3cd7f96d3e6072854afc9e421ab01
William Reynolds <william_reynolds@apple.com>
2017-05-15 13:50:24 -0500

    Wrote a passing view function for posts

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
+
