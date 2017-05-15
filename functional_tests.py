27a810554f2cfcae35e4d3f7526ddfd1204272b4
William Reynolds <william_reynolds@apple.com>
2017-05-15 14:55:05 -0500

    corrected caps on function

--- functional_tests.py
+++ functional_tests.py
@@ -53,7 +53,7 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.AssertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         self.assertIn('2: Use peacock feathers to make a fly', [row.text for
                                                                 row in rows])
 
