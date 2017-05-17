76c4e5b30e614b6a054c2bcb679294f7ff274fd6
Will Reynolds <william_reynolds@apple.com>
2017-05-17 18:24:16 -0500

    changed view for home page to redirect on post

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from lists.models import Item
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text,
-    })
+
+    return render(request, 'home.html')
